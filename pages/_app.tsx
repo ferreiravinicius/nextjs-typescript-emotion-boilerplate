8df4af5dee95b95a49266f2cc55773413ff4a04a
Vinícius Ferreira <vinidotnet@gmail.com>
2021-07-11 03:56:38 -0300

    feat: final config for emotion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import { css, Global } from '@emotion/react'
+
+export const globalStyles = (
+  <Global
+    styles={css`
+      html,
+      body {
+        padding: 0;
+        margin: 0;
+        background: papayawhip;
+        min-height: 100%;
+        font-family: Helvetica, Arial, sans-serif;
+        font-size: 24px;
+        text-align: center;
+      }
+    `}
+  />
+)
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    {globalStyles}
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
